6bd43c21b1f8532b8b6b769200d47e05ad1b0e8f
Marko Musica <marko.musica@emilfreydigital.hr>
2025-11-02 12:13:08 +0100

    Array exercises completed

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if ((typeof a !== 'number') || (typeof b !== 'number') || a < 0 || b < 0 || !Number.isInteger(a) || !Number.isInteger(b)) {
+    return "ERROR";
+  }
 
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+  let sum = 0;
+  for (let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+function round(value, precision = 1) {
+  const multiplier = Math.pow(10, precision || 0);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+const convertToCelsius = function (fahrenheit) {
+  let result = (fahrenheit - 32) * (5 / 9);
+  return round(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celsius) {
+  let result = celsius * (9 / 5) + 32;
+  return round(result);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
